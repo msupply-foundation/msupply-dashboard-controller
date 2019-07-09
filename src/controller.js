dda578e30c2e48540319f5e31a18ab65c8f29145
Will Thomson <will.tho@outlook.com>
2019-07-09 21:01:36 +1200

    Refactor example axios config as method

--- src/controller.js
+++ src/controller.js
@@ -1,26 +1,23 @@
-const axios = require('axios');
+const postDashboard = require('./requests/dashboard.js');
 
-const config = {
-    baseURL: 'localhost::3000',
-    url: 'api/dashboards/db',
-    auth: {
-        username: 'admin',
-        password: 'admin'
-    },
-    method: 'post',
-    data: {
-      dashboard: {
-        id: null,
-        uid: null,
-        title: 'mSupply Demo',
-        tags: [ 'mSupply' ],
-        timezone: 'browser',
-        schemaVersion: 1,
-        version: 1
-      },
-      folderId: 0,
-      overwrite: false
-    }
-}
+const baseURL = 'localhost::3000';
+const auth = { 
+  username: 'admin',
+  password: 'admin'
+};
+const data = {
+  dashboard: {
+    id: null,
+    uid: null,
+    title: 'mSupply Demo',
+    tags: [ 'mSupply' ],
+    timezone: 'browser',
+    schemaVersion: 1,
+    version: 1
+  },
+  folderId: 0,
+  overwrite: false
+};
 
-axios(config).then(res => console.log(res)).catch(error => console.log(error));
+postDashboard(baseURL, auth, data).then(res => console.log(res)).catch(error => console.log(error));
+
